f37353805429d9cb76bec5c3694ff92cc10db4ca
Yuanjing Hong <v1nh1shungry@outlook.com>
2023-09-28 15:10:09 +0800

    update

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -12,8 +12,83 @@ return {
   },
   {
     'echasnovski/mini.ai',
-    dependencies = 'nvim-treesitter/nvim-treesitter-textobjects',
-    event = events.enter_buffer,
+    config = function(_, opts)
+      require('mini.ai').setup(opts)
+      local i = {
+        [' '] = 'Whitespace',
+        ['"'] = 'Balanced "',
+        ["'"] = "Balanced '",
+        ['`'] = 'Balanced `',
+        ['('] = 'Balanced (',
+        [')'] = 'Balanced ) including white-space',
+        ['>'] = 'Balanced > including white-space',
+        ['<lt>'] = 'Balanced <',
+        [']'] = 'Balanced ] including white-space',
+        ['['] = 'Balanced [',
+        ['}'] = 'Balanced } including white-space',
+        ['{'] = 'Balanced {',
+        ['?'] = 'User Prompt',
+        _ = 'Underscore',
+        a = 'Argument',
+        b = 'Balanced ), ], }',
+        c = 'Class',
+        f = 'Function',
+        o = 'Block, conditional, loop',
+        q = "Quote `, \", '",
+        t = 'Tag',
+      }
+      local a = vim.deepcopy(i)
+      for k, v in pairs(a) do
+        a[k] = v:gsub(' including.*', '')
+      end
+      local ic = vim.deepcopy(i)
+      local ac = vim.deepcopy(a)
+      for key, name in pairs({ n = 'Next', l = 'Last' }) do
+        i[key] = vim.tbl_extend('force', { name = 'Inside ' .. name .. ' textobject' }, ic)
+        a[key] = vim.tbl_extend('force', { name = 'Around ' .. name .. ' textobject' }, ac)
+      end
+      require('which-key').register {
+        mode = { 'o', 'x' },
+        i = i,
+        a = a,
+      }
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      dependencies = 'nvim-treesitter/nvim-treesitter',
+      main = 'nvim-treesitter.configs',
+      opts = {
+        textobjects = {
+          swap = {
+            enable = true,
+            swap_next = { ['<M-l>'] = '@parameter.inner' },
+            swap_previous = { ['<M-h>'] = '@parameter.inner' },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              [']a'] = '@parameter.inner',
+              [']f'] = '@function.outer',
+            },
+            goto_next_end = {
+              [']F'] = '@function.outer',
+            },
+            goto_previous_start = {
+              ['[a'] = '@parameter.inner',
+              ['[f'] = '@function.outer',
+            },
+            goto_previous_end = {
+              ['[F'] = '@function.outer',
+            },
+          },
+        },
+      }
+    },
+    keys = {
+      { 'a', mode = { 'x', 'o' } },
+      { 'i', mode = { 'x', 'o' } },
+    },
     opts = function()
       local ai = require('mini.ai')
       return {
@@ -25,6 +100,7 @@ return {
           }, {}),
           f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
           c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {}),
+          t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' },
         },
       }
     end,
@@ -32,10 +108,8 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
     event = events.enter_buffer,
+    main = 'nvim-treesitter.configs',
     opts = {
       ensure_installed = {
         'bash',
@@ -65,36 +139,12 @@ return {
       },
       indent = { enable = true },
       matchup = { enable = true },
-      textobjects = {
-        swap = {
-          enable = true,
-          swap_next = { ['<M-l>'] = '@parameter.inner' },
-          swap_previous = { ['<M-h>'] = '@parameter.inner' },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            [']a'] = '@parameter.inner',
-            [']f'] = '@function.outer',
-          },
-          goto_next_end = {
-            [']F'] = '@function.outer',
-          },
-          goto_previous_start = {
-            ['[a'] = '@parameter.inner',
-            ['[f'] = '@function.outer',
-          },
-          goto_previous_end = {
-            ['[F'] = '@function.outer',
-          },
-        },
-      },
     },
   },
   {
     'Wansmer/treesj',
     config = true,
+    dependencies = 'nvim-treesitter/nvim-treesitter',
     keys = {
       { 'S', '<Cmd>TSJSplit<CR>', desc = 'Split line' },
       { 'J', '<Cmd>TSJJoin<CR>',  desc = 'Join line' },
@@ -103,6 +153,7 @@ return {
   {
     'RRethy/vim-illuminate',
     config = function() require('illuminate').configure { providers = { 'lsp', 'treesitter' } } end,
+    dependencies = 'nvim-treesitter/nvim-treesitter',
     event = events.enter_buffer,
     keys = {
       { '[[', function() require('illuminate').goto_prev_reference(false) end, desc = 'Previous reference' },
@@ -111,11 +162,13 @@ return {
   },
   {
     'lukas-reineke/indent-blankline.nvim',
+    dependencies = 'nvim-treesitter/nvim-treesitter',
     event = events.enter_buffer,
+    main = 'ibl',
     opts = {
-      show_trailing_blankline_indent = false,
-      filetype_exclude = require('utils.ui').excluded_filetypes,
-      show_current_context = true,
+      indent = { char = '│', highlight = 'IndentBlanklineChar' },
+      scope = { enabled = false },
+      exclude = { filetypes = require('utils.ui').excluded_filetypes },
     },
   },
   {
@@ -216,5 +269,12 @@ return {
     'echasnovski/mini.bufremove',
     config = true,
     keys = { { '<C-q>', function() require('mini.bufremove').delete(0, false) end, desc = 'Close buffer' } },
+  },
+  {
+    'RRethy/nvim-treesitter-endwise',
+    dependencies = 'nvim-treesitter/nvim-treesitter',
+    event = 'InsertEnter',
+    main = 'nvim-treesitter.configs',
+    opts = { endwise = { enable = true } },
   },
 }

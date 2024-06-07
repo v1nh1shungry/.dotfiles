af00f3e5eb543cc538910f8a93db7d98b117972c
Yuanjing Hong <v1nh1shungry@outlook.com>
2024-06-07 21:35:18 +0800

    update

---
+++ nvim/lua/dotfiles/plugins/firenvim.lua
@@ -0,0 +1,33 @@
+local specs = {
+  {
+    "glacambre/firenvim",
+    lazy = not vim.g.started_by_firenvim,
+    build = function() vim.fn["firenvim#install"](0) end,
+  },
+}
+
+if not vim.g.started_by_firenvim then
+  return specs
+end
+
+local enabled = vim.tbl_map(function(spec) return spec[1] end, specs)
+for _, module in ipairs({ "core" }) do
+  vim.list_extend(enabled, vim.tbl_map(function(spec) return spec[1] end, require("dotfiles.plugins." .. module)))
+end
+
+local Config = require("lazy.core.config")
+Config.options.checker.enabled = false
+Config.options.change_detection.enabled = false
+Config.options.defaults.cond = function(plugin) return vim.list_contains(enabled, plugin[1]) end
+
+vim.g.firenvim_config = {
+  localSettings = {
+    [".*"] = { cmdline = "neovim" },
+  },
+}
+
+local map = require("dotfiles.utils.keymap")
+map({ "<C-z>", "<Cmd>call firenvim#hide_frame()<CR>", desc = "Hide neovim frame" })
+map({ "<Esc><Esc>", "<Cmd>call firenvim#focus_page()<CR>", desc = "Back to the page" })
+
+return specs

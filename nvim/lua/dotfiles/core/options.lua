3551cf32b2a4f06314f9d78f3188a4ffe3c54524
Yuanjing Hong <v1nh1shungry@outlook.com>
2024-08-01 13:41:33 +0800

    update

--- nvim/lua/dotfiles/core/options.lua
+++ nvim/lua/dotfiles/core/options.lua
@@ -1,19 +1,3 @@
-if vim.fn.has("wsl") == 1 then
-  vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
-end
-
-vim.opt.clipboard = "unnamedplus"
 vim.opt.colorcolumn = "80"
 vim.opt.confirm = true
 vim.opt.cursorline = true

88f52515f4745d92a6c4cdb9778ffa72b0bc74ce
Yuanjing Hong <v1nh1shungry@outlook.com>
2024-08-01 13:41:33 +0800

    update

--- nvim/lua/dotfiles/plugins/ui.lua
+++ nvim/lua/dotfiles/plugins/ui.lua
@@ -267,7 +267,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      preset = "modern",
+      preset = "helix",
       spec = {
         {
           mode = { "n", "v" },

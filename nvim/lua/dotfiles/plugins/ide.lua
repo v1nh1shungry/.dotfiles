3531894bb270e1b72ed6f793d1ddf3c27f5867a0
Yuanjing Hong <v1nh1shungry@outlook.com>
2024-09-06 15:29:25 +0800

    update

--- nvim/lua/dotfiles/plugins/ide.lua
+++ nvim/lua/dotfiles/plugins/ide.lua
@@ -272,49 +272,8 @@ return {
   -- }}}
   {
     "hrsh7th/nvim-cmp",
-    config = function(_, opts)
-      local cmp = require("cmp")
-
-      cmp.setup(opts)
-
-      local cmdline_mapping = {
-        ["<Tab>"] = {
-          c = function()
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              cmp.complete()
-            end
-          end,
-        },
-        ["<S-Tab>"] = {
-          c = function()
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              cmp.complete()
-            end
-          end,
-        },
-        ["<C-e>"] = { c = cmp.mapping.abort() },
-      }
-
-      cmp.setup.cmdline(":", {
-        mapping = cmdline_mapping,
-        sources = cmp.config.sources({
-          { name = "cmdline" },
-          { name = "path" },
-        }),
-      })
-
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmdline_mapping,
-        sources = cmp.config.sources({ { name = "buffer" } }),
-      })
-    end,
     dependencies = {
       "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-path",
       "lukas-reineke/cmp-rg",
     },
@@ -392,35 +351,9 @@ return {
 
       dapui.setup()
 
-      dap.listeners.before.attach.dapui_config = dapui.open
-      dap.listeners.before.launch.dapui_config = dapui.open
+      dap.listeners.after.event_initialized.dapui_config = dapui.open
       dap.listeners.before.event_terminated.dapui_config = dapui.close
       dap.listeners.before.event_exited.dapui_config = dapui.close
-
-      local debug_mode = Layers.mode.new()
-      debug_mode:auto_show_help()
-      dap.listeners.after.event_initialized.debug_mode = function()
-        debug_mode:activate()
-      end
-      dap.listeners.before.event_terminated.debug_mode = function()
-        debug_mode:deactivate()
-      end
-      dap.listeners.before.event_exited.debug_mode = function()
-        debug_mode:deactivate()
-      end
-      debug_mode:keymaps({
-        n = {
-          { "c", "<Cmd>DapContinue<CR>", { desc = "Continue" } },
-          { "C", function() dap.run_to_cursor() end, { desc = "Run to cursor" } },
-          { "s", "<Cmd>DapStepInto<CR>", { desc = "Step into" } },
-          { "o", "<Cmd>DapStepOut<CR>", { desc = "Step out" } },
-          { "n", "<Cmd>DapStepOver<CR>", { desc = "Step over" } },
-          { "d", function() dap.down() end, { desc = "Go down in current stacktrace" } },
-          { "u", function() dap.up() end, { desc = "Go up in current stacktrace" } },
-          { "K", function() dapui.eval() end, { desc = "Eval" } },
-          { "q", "<Cmd>DapTerminate<CR>", { desc = "Terminate" } },
-        },
-      })
     end,
     dependencies = {
       "nvim-neotest/nvim-nio",
@@ -440,14 +373,18 @@ return {
         "theHamsta/nvim-dap-virtual-text",
         opts = {},
       },
-      {
-        "debugloop/layers.nvim",
-        opts = {},
-      },
     },
     keys = {
       { "<Leader>db", "<Cmd>DapToggleBreakpoint<CR>", desc = "Toggle breakpoint" },
       { "<Leader>dc", "<Cmd>DapContinue<CR>", desc = "Continue" },
+      { "<Leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to cursor" },
+      { "<Leader>dn", "<Cmd>DapStepOver<CR>", desc = "Step over" },
+      { "<Leader>ds", "<Cmd>DapStepInto<CR>", desc = "Step into" },
+      { "<Leader>do", "<Cmd>DapStepOut<CR>", desc = "Step out" },
+      { "<Leader>dd", function() require("dap").down() end, desc = "Go down in current stacktrace" },
+      { "<Leader>du", function() require("dap").up() end, desc = "Go up in current stacktrace" },
+      { "<Leader>dt", "<Cmd>DapTerminate<CR>", desc = "Terminate" },
+      { "<Leader>dK", function() require("dapui").eval() end, desc = "Eval" },
     },
   },
   {

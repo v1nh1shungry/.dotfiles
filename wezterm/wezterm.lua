0a4e06c2ea48680ff380db8053ac1404066eaacb
Yuanjing Hong <v1nh1shungry@outlook.com>
2024-09-22 14:50:46 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,13 +7,14 @@ end)
 
 return {
   color_scheme = "Tokyo Night Storm",
+  default_prog = { "tmux" },
+  disable_default_key_bindings = true,
   font = wezterm.font_with_fallback({
     "JetBrainsMono Nerd Font Mono",
     "Noto Sans CJK SC",
   }),
   font_size = 14,
-  default_prog = { "tmux" },
-  disable_default_key_bindings = true,
+  front_end = "WebGpu",
   hide_tab_bar_if_only_one_tab = true,
   keys = {
     { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
@@ -25,5 +26,4 @@ return {
     { key = "Tab", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1) },
     { key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
   },
-  window_decorations = "NONE",
 }

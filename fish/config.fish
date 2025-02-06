d0e21ae6ba0646a4dfc526ff2096ffae8e12a68e
Yuanjing Hong <v1nh1shungry@outlook.com>
2025-02-07 01:12:18 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,7 @@
 if status is-interactive
     set -gx LANG "en_US.UTF-8"
     set -gx LANGUAGE "en_US.UTF-8"
-    set -gx LC_NAME "en_US.UTF-8"
-    set -gx LC_TIME "en_US.UTF-8"
-    set -gx LC_PAPER "en_US.UTF-8"
-    set -gx LC_ADDRESS "en_US.UTF-8"
-    set -gx LC_NUMERIC "en_US.UTF-8"
-    set -gx LC_MONETARY "en_US.UTF-8"
-    set -gx LC_TELEPHONE "en_US.UTF-8"
-    set -gx LC_MEASUREMENT "en_US.UTF-8"
-    set -gx LC_IDENTIFICATION "en_US.UTF-8"
+    set -gx LC_ALL "en_US.UTF-8"
 
     set fish_greeting
 
@@ -17,7 +9,9 @@ if status is-interactive
         fish_add_path ~/.local/bin
     end
 
-    if test -d ~/.cargo
+    if test -d ~/.rustup || command -q rustup
+        set -gx RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+        set -gx RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
         fish_add_path ~/.cargo/bin
     end
 
@@ -32,10 +26,6 @@ if status is-interactive
     if command -q fnm
         fnm env --use-on-cd | source
         set -gx FNM_NODE_DIST_MIRROR https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
-    end
-
-    if command -q bob
-        fish_add_path ~/.local/share/bob/nvim-bin
     end
 
     if command -q nvim

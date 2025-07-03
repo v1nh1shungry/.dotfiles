eac2b34ec6afb57048b9475a13177afe7dde08b1
Yuanjing Hong <v1nh1shungry@outlook.com>
2025-07-03 18:59:12 +0800

    update

---
+++ nvim/lua/dotfiles/plugins/extra/leetcode.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "MunifTanjim/nui.nvim",
+      "folke/snacks.nvim",
+    },
+    lazy = vim.fn.argv()[1] ~= "leetcode.nvim",
+    opts = {
+      cn = { enabled = true },
+      injector = {
+        cpp = {
+          imports = function() return { "#include <bits/stdc++.h>", "using namespace std;" } end,
+        },
+      },
+      picker = "snacks-picker",
+    },
+  },
+}

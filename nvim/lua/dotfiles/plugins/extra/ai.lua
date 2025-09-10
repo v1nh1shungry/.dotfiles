41ff040f8689b3991f5658faa3b2c5c33bf821e6
Yuanjing Hong <v1nh1shungry@outlook.com>
2025-09-11 01:20:01 +0800

    update

--- nvim/lua/dotfiles/plugins/extra/ai.lua
+++ nvim/lua/dotfiles/plugins/extra/ai.lua
@@ -6,14 +6,6 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    enabled = function()
-      if not vim.env.CODECOMPANION_NVIM_API_KEY then
-        Dotfiles.notify.warn("`CODECOMPANION_NVIM_API_KEY` is not set")
-        return false
-      end
-
-      return true
-    end,
     keys = {
       { "<Leader>a", "", desc = "+ai" },
       { "<Leader>aa", "<Cmd>CodeCompanionChat Toggle<CR>", desc = "Chat" },
@@ -21,27 +13,12 @@ return {
       { "<Leader>a/", "<Cmd>CodeCompanionActions<CR>", desc = "Actions" },
     },
     opts = {
-      adapters = {
-        qwen = function()
-          return require("codecompanion.adapters").extend("openai_compatible", {
-            env = {
-              url = "https://dashscope.aliyuncs.com/compatible-mode",
-              api_key = "CODECOMPANION_NVIM_API_KEY",
-            },
-            schema = {
-              model = {
-                default = "qwen-plus-2025-07-28",
-              },
-            },
-          })
-        end,
-      },
       opts = {
         language = "Chinese",
       },
       strategies = {
         chat = {
-          adapter = "qwen",
+          adapter = "gemini_cli",
         },
       },
     },

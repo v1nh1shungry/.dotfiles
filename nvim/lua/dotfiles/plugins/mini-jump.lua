c4fa6f8ecdea38ed5ffb26cecfac512b52af199a
Yuanjing Hong <v1nh1shungry@outlook.com>
2026-07-30 17:51:28 +0800

    update

---
+++ nvim/lua/dotfiles/plugins/mini-jump.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-mini/mini.jump",
+    keys = {
+      { "f", mode = { "n", "x", "o" } },
+      { "F", mode = { "n", "x", "o" } },
+      { "t", mode = { "n", "x", "o" } },
+      { "T", mode = { "n", "x", "o" } },
+      { ";", mode = { "n", "x", "o" } },
+    },
+    opts = {},
+  },
+}

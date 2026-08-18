718570816c0b5b4cd387960c2fb7cfdd105bd387
Yuanjing Hong <v1nh1shungry@outlook.com>
2026-08-18 18:43:19 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -23,11 +23,6 @@ if command -q mise
     mise activate fish | source
 end
 
-if command -q cmake
-    set -x CMAKE_EXPORT_COMPILE_COMMANDS "ON"
-    set -x CMAKE_GENERATOR "Ninja"
-end
-
 if command -q eza
     alias ls "eza --icons auto"
 end
